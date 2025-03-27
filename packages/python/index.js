82d01af7cc9ec3a1cbbc40ec514b7f2814a2bf2f
Peli de Halleux <pelikhan@users.noreply.github.com>
2025-03-27 03:47:55 +0000

    pr feedback

--- packages/python/index.js
+++ packages/python/index.js
@@ -4,6 +4,6 @@ const libPath = path.join(__dirname, 'parser.so')
 module.exports = {
   libraryPath: libPath,
   extensions: ["py"],
-  languageSymbol: 'tree_sitter_Python',
+  languageSymbol: 'tree_sitter_python',
   expandoChar: 'µ',
 }
